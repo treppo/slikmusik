cce217491230ede92a86935521c4c12d1a1a95fd
treppo <christian@treppo.org>
2014-04-20 13:46:19 +0100

    Add user login and logout

--- SlikMusik/Controllers/UserController.cs
+++ SlikMusik/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using SlikMusik.Identity;
 using SlikMusik.Models;
 
@@ -16,6 +17,7 @@ namespace SlikMusik.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create(CreateUser model)
         {
             if (ModelState.IsValid)
@@ -31,6 +33,49 @@ namespace SlikMusik.Controllers
                 AddErrorsFromResult(result);
             }
             return View(model);
+        }
+
+        public ActionResult Login(string returnUrl = "/")
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginUser userDetails, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserManager.FindAsync(userDetails.Name, userDetails.Password);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Invalid email or password.");
+                }
+                else
+                {
+                    var ident =
+                        await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties {IsPersistent = false}, ident);
+                    return Redirect(returnUrl);
+                }
+            }
+
+            return View(userDetails);
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
         }
 
         private void AddErrorsFromResult(IdentityResult result)
---
+++ SlikMusik/Models/LoginUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlikMusik.Models
+{
+    public class LoginUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
