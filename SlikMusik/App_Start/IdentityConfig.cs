ba336838a6400b0e01b5a0bf7740d4d432de17b9
treppo <christian@treppo.org>
2014-04-20 11:46:01 +0100

    Move the Identity configuration to the web ui

---
+++ SlikMusik/App_Start/IdentityConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using SlikMusik.Identity;
+
+namespace SlikMusik
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(IdentityDbContext.Create);
+            app.CreatePerOwinContext<UserManager>(UserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/User/Login"),
+            });
+        }
+    }
+}
